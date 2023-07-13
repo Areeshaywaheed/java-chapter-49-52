f070f7474fdcbf8b47dd63b397dc8951dde3ca48
Areeshaywaheed <128454186+Areeshaywaheed@users.noreply.github.com>
2023-07-13 20:43:16 +0500

    Add files via upload

---
+++ java chapter 49 to 52/js/app.js
@@ -0,0 +1,34 @@
+         //  ============chapter 49-52 started===========
+
+    //   ==========question number 1==========
+
+// function signupForm() {
+//     alert(document.getElementById("input1").value);
+//     alert(document.getElementById("input2").value);
+// }
+
+        //   ===========question number 2========
+
+// function inhancePara() {
+//     var paragraph = "As a web developer, I am a passionate and highly skilled professional who thrives on bringing creative ideas to life in the digital world. With a solid foundation in programming languages such as HTML, CSS, and JavaScript, I possess the technical expertise necessary to build visually appealing and user-friendly websites. I have a keen eye for design and a deep understanding of user experience, allowing me to craft intuitive interfaces and seamless navigation. Additionally, I am well-versed in various web development frameworks and libraries, enabling me to efficiently develop scalable and robust web applications. I constantly stay updated with the latest trends and advancements in web development to ensure that I deliver cutting-edge solutions to my clients.Moreover, I am a strong communicator and collaborate effectively with cross-functional teams, translating complex technical concepts into easily understandable terms. With a detail-oriented approach and a commitment to delivering high-quality work, I strive to create impactful digital experiences that leave a lasting impression on users.";
+
+//     document.getElementById("CakeWebsite").innerHTML = paragraph;
+// }
+
+        // ========question number 3=========
+
+// function deletedRow(btn) {
+//     var row = btn.parentNode.parentNode;
+//     console.log(row);
+//     var rowIndex = row.rowIndex;
+//     console.log(rowIndex);
+//     row.parentNode.removeChild(row);
+
+//     var table = document.getElementsByTagName("table")[0];
+//     var rowCount = table.rows.length;
+
+//     for (var i = rowIndex; i < rowCount; i++) {
+//         var row = table.rows[i];
+//         row.cells[0].textContent = i - 1;
+//     }
+// }
